a942d4fbb419f708ffa423b4b714bc0bf17f0161
LAPTOP-QMITUAI4\hluca <lucas.hureaux@etudiant.univ-reims.fr>
2024-11-20 10:18:55 +0100

    add the code to select the time limit

--- script.js
+++ script.js
@@ -3,13 +3,35 @@ let clics = 0;
 let cps = 0;
 
 const button = document.querySelector(".cps")
+const timers = document.querySelectorAll(".buttonsList button")
+localStorage.setItem("timer", 5)
+
+timers.forEach((timer) => {
+  timer.addEventListener("click", () => {
+    console.log(timer.classList.value)
+    switch(timer.classList.value) {
+      case "5s noleft" :
+        localStorage.setItem("timer", 5)
+        break;
+      case "10s" :
+        localStorage.setItem("timer", 10)
+        break;
+      case "30s" :
+        localStorage.setItem("timer", 30)
+        break;
+      case "1m" :
+        localStorage.setItem("timer", 60)
+        break;
+    }
+  })
+})
 
 button.addEventListener("click", () => {
   if (button.innerHTML === "Cliquez sur moi!") {
-    timer = 10;
+    timer = localStorage.getItem("timer");
     document.getElementById("message").classList.remove("hide")
     document.getElementById("message").innerHTML = timer;
-    startTimer();
+    startTimer(timer);
   }
   if (timer >= 0) {
     clics += 1;
@@ -17,15 +39,16 @@ button.addEventListener("click", () => {
   }
 })
 
-function startTimer() {
+function startTimer(timerStorage) {
   setTimeout(() => {
     if (timer === 0) {
+      console.log(timerStorage)
       timer = -1;
-      cps = clics/10;
+      cps = clics/timerStorage;
       document.getElementById("message").innerHTML = "Temps écoulé! Score : "+cps.toFixed(1)+" clics par seconde";
     } else {
       timer -= 1;
-      startTimer();
+      startTimer(timerStorage);
       document.getElementById("message").innerHTML = timer;
     }
   }, 1000)
